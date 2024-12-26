844fa0880726fe5f8e521a288986f1fc5bc717c7
KaungMoeSetTAT <kaungmoeset@thuriyaacetechnology.com>
2024-12-26 23:36:31 +0630

    Solved Target Sum Problem

--- DayOneLeet.java
+++ DayOneLeet.java
@@ -1,2 +1,21 @@
 public class DayOneLeet {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        return findTargetSumWays(nums, target, 0, 0);
+    }
+
+    private int findTargetSumWays(int[] nums, int target, int index, int sum) {
+        if (index == nums.length) {
+            return sum == target ? 1 : 0;
+        }
+        return findTargetSumWays(nums, target, index + 1, sum + nums[index]) +
+                findTargetSumWays(nums, target, index + 1, sum - nums[index]);
+    }
+
+    public static void main(String[] args) {
+        DayOneLeet dayOneLeet = new DayOneLeet();
+        int[] nums = {1, 1, 1, 1, 1};
+        int target = 3;
+        System.out.println(dayOneLeet.findTargetSumWays(nums, target));
+    }
 }
