197b1279dd21ca8ddb4172a3b8e79c8e5735e392
KaungMoeSetTAT <kaungmoeset@thuriyaacetechnology.com>
2024-12-27 16:17:57 +0630

    Solve Final Pice with Special Discount in a shop

--- FinalPriceWithSpecialDiscount.java
+++ FinalPriceWithSpecialDiscount.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class FinalPriceWithSpecialDiscount {
+
+    private static int[] finalPrices(int[] prices) {
+        int[] result = new int[prices.length];
+        for (int i = 0; i < prices.length; i++) {
+            int discount = 0;
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] <= prices[i]) {
+                    discount = prices[j];
+                    break;
+                }
+            }
+            result[i] = prices[i] - discount;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {8, 4, 6, 2, 3};
+        int[] result = finalPrices(prices);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
+    }
 }
